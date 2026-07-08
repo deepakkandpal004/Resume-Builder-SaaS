5ab9f1330cc12ca9a502c593ff724fe0c91172c4
deepakkandpal004 <deepakkandpal004@users.noreply.github.com>
2026-07-08 09:21:10 +0530

    feat: update logo to logo.svg and favicon to favicon.ico

--- client/src/components/Logo.jsx
+++ client/src/components/Logo.jsx
@@ -1,6 +1,6 @@
 const Logo = ({ className = "h-12 w-auto" }) => (
   <img
-    src="/logo.png"
+    src="/logo.svg"
     alt="Resume AI"
     className={className}
     style={{ objectFit: "contain" }}
