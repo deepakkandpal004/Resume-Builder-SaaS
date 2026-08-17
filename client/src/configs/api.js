877166c8d6ced2c08f18fc14b86b6c010bdeb627
Deepak Kandpal <deepakkandpal004@users.noreply.github.com>
2026-08-17 21:11:14 +0530

    fixes firebase issue

--- client/src/configs/api.js
+++ client/src/configs/api.js
@@ -1,17 +1,29 @@
 import axios from "axios";
 import { auth } from "../config/firebase";
+import { onAuthStateChanged } from "firebase/auth";
 
 let cachedToken = null;
+let authReady = new Promise((resolve) => {
+  onAuthStateChanged(auth, async (user) => {
+    if (user) {
+      cachedToken = await user.getIdToken();
+    }
+    resolve();
+  });
+});
 
 export const setCachedToken = (token) => {
   cachedToken = token;
 };
+
+export const waitForAuth = () => authReady;
 
 const api = axios.create({
     baseURL: import.meta.env.VITE_BASE_URL
 });
 
 api.interceptors.request.use(async (config) => {
+    await authReady;
     const user = auth.currentUser;
     if (user) {
         const token = await user.getIdToken();
